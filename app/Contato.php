a734c40676d12526d7d00eb05ccb2dd0b0562b76
Ronie <roniemeque@outlook.com>
2019-03-25 21:38:56 -0300

    Configurando campos preenchíveis do model

--- app/Contato.php
+++ app/Contato.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contato extends Model
 {
-    //
+    //definindo campos que podem ser atribuidos em massa
+    protected $fillable = [
+        'nome', 'email', 'telefone', 'mensagem', 'path_arquivo', 'ip_acesso'
+    ];
 }
